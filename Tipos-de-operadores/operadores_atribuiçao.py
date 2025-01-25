2c391ca5c841cbb316535d7f020ccb95b690d3aa
rafacode404 <tvmarte17@gmail.com>
2025-01-24 23:11:49 -0300

    Create operadores_atribuiçao.py

---
+++ Tipos-de-operadores/operadores_atribuiçao.py
@@ -0,0 +1,32 @@
+# Inicializa a variável saldo com o valor 200
+saldo = 200
+print(saldo)  
+
+# Multiplica saldo por 20
+saldo *= 20
+print(saldo)  
+
+# Realiza a divisão inteira de saldo por 10
+saldo //= 10
+print(saldo)  
+
+# Subtrai 100 de saldo
+saldo -= 100
+print(saldo)  #
+# Divide saldo por 5
+saldo /= 5
+print(saldo)  
+
+# Adiciona 10 a saldo
+saldo += 10
+print(saldo)  
+
+# Eleva saldo ao quadrado
+saldo **= 2
+print(saldo)    
+
+# Calcula o resto da divisão de saldo por 200
+saldo %= 200
+print(saldo)  
+
+
