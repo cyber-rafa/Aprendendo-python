e45f0ff0d0dfab9f0f81ebaf3ab96f57315b71c0
rafacode404 <tvmarte17@gmail.com>
2025-01-24 21:39:57 -0300

    up

---
+++ funçoes_entrada_saida.py
@@ -0,0 +1,8 @@
+nome = input('Informe seu nome:')
+
+idade = input('informe sua idade ')
+
+print(nome, idade)
+print(nome, idade, end='... \n')
+print(nome, idade, sep='#', end='...\n')
+print(nome, idade, sep='#')
