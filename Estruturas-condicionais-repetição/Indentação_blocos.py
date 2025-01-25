8ff004e687faa8c396cfbac00e52ca83f348550a
rafacode404 <tvmarte17@gmail.com>
2025-01-25 14:36:19 -0300

    UP

--- Estruturas-condicionais-repetição/Indentação_blocos.py
+++ Estruturas-condicionais-repetição/Indentação_blocos.py
@@ -1,1 +1,13 @@
-print()
+def sacar(valor):
+    saldo = 100
+
+    if saldo >= valor:
+        print('Saque efetuado com sucesso')
+        print('Saldo atual: ', saldo - valor)
+
+    if saldo < valor:
+        print('Saldo insuficiente')
+        print('Saldo atual' , saldo)
+          
+
+sacar(110)
