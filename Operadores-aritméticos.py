44f794f1484b363141e2a72e28ccab1866dc0e82
rafacode404 <tvmarte17@gmail.com>
2025-01-24 22:17:27 -0300

    Update Operadores-aritméticos.py

--- Operadores-aritméticos.py
+++ Operadores-aritméticos.py
@@ -1,1 +1,22 @@
-print()
+produto_1 = 30
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 * produto_2)
+print(produto_1 / produto_2)
+
+#divisao inteira do produto
+print(produto_1 // produto_2)
+
+# resto divisao de produto
+print(produto_1 % produto_2)
+
+#exponenciaçao do produto
+print(produto_1 ** produto_2)
+
+
+x = (2 // 1) + (10 * 3) - (20 % 5)
+y = (2 // 1) + 2 - ((10 * 3 ) + 5)
+print(x)
+print(y)
