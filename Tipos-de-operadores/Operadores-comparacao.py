03d88930540f2a74cbc2bb23119dec11f6015ff6
rafacode404 <tvmarte17@gmail.com>
2025-01-24 22:49:01 -0300

    up

---
+++ Tipos-de-operadores/Operadores-comparacao.py
@@ -0,0 +1,21 @@
+# Definindo o valor da variável saque
+saque = 450
+saque = 450
+
+# Comparando se saque é igual a saque
+print(saque == saque)
+
+# Comparando se saque é diferente de saque
+print(saque != saque)
+
+# Comparando se saque é maior que saque
+print(saque > saque)
+
+# Comparando se saque é menor que saque
+print(saque < saque)
+
+# Comparando se saque é maior ou igual a saque
+print(saque >= saque)
+
+# Comparando se saque é menor ou igual a saque
+print(saque <= saque)
