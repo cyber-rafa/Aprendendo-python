2d9b283c0121f5654b3ac7a1f3d25b32ee5244ad
rafacode404 <tvmarte17@gmail.com>
2025-01-25 10:58:06 -0300

    up

---
+++ Tipos-de-operadores/operadores_identidade.py
@@ -0,0 +1,13 @@
+nomes = 'Rafael, Kaique, Arthur'
+carros = 'Ferrari, Lamborghini, Porsche'
+saques = [100, 200, 300]
+
+
+name = 'Daniel'in nomes
+print(name) 
+
+car = 'tesla' not in carros
+print(car)
+
+saque = 1000 in saques
+print(saque)
