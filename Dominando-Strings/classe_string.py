0c9c03599939a27377b3bda218e053ea7b8d2f8c
rafacode404 <tvmarte17@gmail.com>
2025-01-26 17:22:27 -0300

    up

--- Dominando-Strings/classe_string.py
+++ Dominando-Strings/classe_string.py
@@ -1,1 +1,16 @@
-print('hello world')
+name  = ' RaFaeL   '
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+
+texto = '   Rio de Janeiro     '
+print(texto.strip())
+print(texto.lstrip()  + '-')
+print(texto.rstrip() + '-')
+
+carros = 'gol'
+print( '###' + carros + '###')
+print(carros.center(10, '#'))
+print('-' .join(carros))
