1849570a03321cca1fa6dac6bfceb773eb3716f1
rafacode404 <tvmarte17@gmail.com>
2025-01-27 10:50:34 -0300

    up

--- Dominando-Strings/Fatiamento_string.py
+++ Dominando-Strings/Fatiamento_string.py
@@ -3,3 +3,8 @@ name =  'Daniel Barbosa de oliveira'
 print(name[:6])
 print(name[:: -1])
 print(name[2:10])
+print(name[-1])
+
+# Prints characters from index 3 to 9, skipping every second character
+print(name[3:10:2])  
+
