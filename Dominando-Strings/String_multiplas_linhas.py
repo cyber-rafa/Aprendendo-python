dd3f453f5fcdc975e6905726f625cd39d1a15159
rafacode404 <tvmarte17@gmail.com>
2025-01-27 20:41:21 -0300

    up

---
+++ Dominando-Strings/String_multiplas_linhas.py
@@ -0,0 +1,20 @@
+name = 'Kali linux'
+
+mensagem = f''''
+
+  O sistema operacional dos hackes {name}
+'''''
+
+print(mensagem)
+
+print(
+    ''' --------- Conta -------
+    
+    1 - sacar 
+
+    2 - Depositar
+
+    3 - sair
+    
+    '''
+)
