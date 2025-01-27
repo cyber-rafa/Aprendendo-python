3bf0a8184334e9bd3163bc12cf1e4273a73fb8b1
rafacode404 <tvmarte17@gmail.com>
2025-01-27 10:56:54 -0300

    up

--- Dominando-Strings/Fatiamento_string.py
+++ Dominando-Strings/Fatiamento_string.py
@@ -4,7 +4,7 @@ print(name[:6])
 print(name[:: -1])
 print(name[2:10])
 print(name[-1])
+print(name[:])
 
 # Prints characters from index 3 to 9, skipping every second character
 print(name[3:10:2])  
-
