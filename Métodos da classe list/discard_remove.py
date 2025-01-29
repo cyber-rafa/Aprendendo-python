cbaaee9e38f998bdec4fffdc6678f502f18f1d2e
rafacode404 <tvmarte17@gmail.com>
2025-01-29 14:39:26 -0300

    up

---
+++ Métodos da classe list/discard_remove.py
@@ -0,0 +1,17 @@
+numeros = {1, 2, 3, 4, 5 , 5, 6, 7, 7, 8, 9, 10}
+
+
+numeros.discard(1)
+numeros.discard(45)
+
+print(numeros)
+
+numeros = {1, 2, 3, 4, 5 , 5, 6, 7, 7, 8, 9, 10}
+
+numeros.remove(5)
+numeros.remove(2)
+
+# o remove() gera um erro caso o valor passado como parametro não exista no conjunto
+numeros.remove(45)
+
+print(numeros)
