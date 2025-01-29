d4d9b5991cfce1e08fef1944343b5986b4f118db
rafacode404 <tvmarte17@gmail.com>
2025-01-29 11:05:15 -0300

    up

---
+++ Métodos da classe list/remove.py
@@ -0,0 +1,20 @@
+linguagens = ['Python', 'Java', 'C#' , 'csharp' , 'Java']
+
+# Remove the first occurrence of 'Java' from the list
+linguagens.remove('Java')
+
+# Remove all occurrences of 'Java' from the list
+while 'Java' in linguagens:
+    linguagens.remove('Java')
+
+print(linguagens)
+
+# --------------------------------------------- ! --------------------------------------------- #
+
+
+linguagens = ['Python', 'Java', 'C#' , 'csharp' , 'Java']
+
+# Remove the first occurrence of 'Java' from the list
+linguagens.remove('Java')
+
+print(linguagens)
