ecbad1e4223e18fae9e920d4cf4a949d3c6e1029
rafacode404 <tvmarte17@gmail.com>
2025-02-08 16:22:45 -0300

    up

--- funçoes/funçoes.py
+++ funçoes/funçoes.py
@@ -3,4 +3,7 @@ def salvar_carro(modelo, marca, ano, placa):
     print(f'Carro inserido com sucesso! {modelo}/{marca}/{ano}/{placa}')
 
 
+
+
 salvar_carro('Fiat', 'palio', '1991', 'EA-221')
+salvar_carro(**{'modelo': 'palio',  'marca': 'fiat', 'ano': '1992' , 'placa': 'TR5-OP' })
