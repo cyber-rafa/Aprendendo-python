6a0e9376f6bf6b1ce706abd9af925a1f2e2f8365
rafacode404 <tvmarte17@gmail.com>
2025-02-08 16:56:19 -0300

    up

---
+++ funçoes/args.py
@@ -0,0 +1,15 @@
+def exibir_poema(data_extenso, *lista, **dicionario):
+    texto = '\n'.join(lista)
+    meta_dados = '\n'.join([f'{chave.title()}]: {valor}' for chave, valor in dicionario.items()])
+    mensagem =  f'{data_extenso}\n\n{texto}\n\n{meta_dados}'
+    print(mensagem)
+
+    exibir_poema(
+        'sabado, 01, de janeiro de 1998',
+        'O poeta é um fingidor',
+        'Finge tão completamente',
+        'Que chega a fingir que é dor',
+        'A dor que deveras sente.',
+        autor='desconhecido',
+        ano=1,
+    )
