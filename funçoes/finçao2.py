bb298b594e632ae8edf605a98da725a6092a0136
rafacode404 <tvmarte17@gmail.com>
2025-02-18 16:08:46 -0300

    up

--- funçoes/finçao2.py
+++ funçoes/finçao2.py
@@ -1,4 +1,8 @@
 def criar_carro(modelo, ano, placa, /, *,marca, motor, combustivel):
     print(modelo, ano, placa, marca, motor, combustivel)
 
+
 criar_carro('uno', 1991, 'ABC-123', marca='fiat', motor='1.0', combustivel='gas')
+
+
+#criar_carro(modelo='uno', ano=1991, placa='ABC-123', marca='fiat', motor='1.0', combustivel='gas')
---
+++ funçoes/global.py
@@ -0,0 +1,9 @@
+salario = 3000
+
+def salario_bonus(bonus):
+    global salario
+    salario += bonus
+    return salario
+
+salario_com_bonus = salario_bonus(500)
+print(salario_com_bonus)
