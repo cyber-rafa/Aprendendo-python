5569c09add428ffefcb772006396c8ba9f34a9d4
rafacode404 <tvmarte17@gmail.com>
2025-02-25 11:34:45 -0300

    up

--- Orientaçao_a_objetos/poo.py
+++ Orientaçao_a_objetos/poo.py
@@ -2,5 +2,20 @@ class bicicleta:
     def __init__(self, marca, modelo, cor, valor):
         self.marca = marca
         self.modelo = modelo
-        self,cor = cor
+        self.cor = cor
         self.valor = valor
+
+    
+    def pedalar(self):
+        print('pedalando')
+
+    def buzinar(self):
+        print('bi bi')
+
+    def para(self):
+        print('parando')
+        print('parou')
+
+
+b1 = bicicleta('caloi', 'elite', 'vermelha', 1000)
+print(b1.marca)
