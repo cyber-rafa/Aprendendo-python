cf39c35f14720dbf57dd08067def6b9e924f520d
rafacode404 <tvmarte17@gmail.com>
2025-02-27 15:37:00 -0300

    up

---
+++ herança/herança_multiplica.py
@@ -0,0 +1,21 @@
+class animal:
+    pass
+
+class mamifero(animal):
+    pass
+
+class cachorro(mamifero):
+    pass
+
+class gato(mamifero):
+    pass
+
+class leao(animal):
+    pass
+
+
+class ave(animal):
+    pass
+
+class onitorrinco(animal, mamifero, ave):
+    pass
