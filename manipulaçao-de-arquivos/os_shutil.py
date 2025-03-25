c727b7b90730226903e4fa3719f0c30c673eaf98
cyber-rafa <tvmarte17@gmail.com>
2025-03-25 12:16:33 -0300

    up

--- manipulaçao-de-arquivos/os_shutil.py
+++ manipulaçao-de-arquivos/os_shutil.py
@@ -5,3 +5,12 @@ from pathlib import Path
 ROO_PATH = Path(__file__).parent
 
 os.mkdir(ROO_PATH / 'novo-diretorio')
+
+arquivo = open(ROO_PATH  / 'novo.txt', 'w')
+arquivo.close()
+
+os.rename(ROO_PATH / 'novo.txt', ROO_PATH / 'alterado.txt')
+
+os.remove(ROO_PATH / 'novo.txt')
+
+shutil.move(ROO_PATH / 'alterado.txt', ROO_PATH / 'novo-diretorio' / 'alterado.txt')
